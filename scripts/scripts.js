3bfb6b54734eee7f4059bcbf82d754aa45703348
Sergei Shchegolev <schego1ev@yandex-team.ru>
2021-09-17 15:50:48 +0300

    prifile popup complite

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,13 +7,19 @@ const jobInput = document.querySelector('#profile-profession');
 const profileName = document.querySelector('.profile-info__name');
 const profileJob = document.querySelector('.profile-info__profession');
 
+
 profileEditButton.addEventListener('click', () => {
   popup.classList.add('popup_opened');
 });
 
-popupCloseButton.addEventListener('click', () => {
+let popupClose = () => {
   popup.classList.remove('popup_opened');
-});
+}
+
+popupCloseButton.addEventListener('click', () => {
+   popupClose();
+   getProfileInfo();
+  });
 
 let getProfileInfo = () => {
   nameInput.value = profileName.textContent;
@@ -21,12 +27,40 @@ let getProfileInfo = () => {
 }
 getProfileInfo();
 
+
 function formSubmitHandler(evt) {
   evt.preventDefault();
   const nameValue = nameInput.value;
   const jobValue = jobInput.value;
   profileName.textContent = nameValue;
   profileJob.textContent = jobValue;
+  popupClose();
 }
-
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
